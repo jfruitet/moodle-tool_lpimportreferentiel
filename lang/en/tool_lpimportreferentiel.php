fc53e48fb2f1b083818e15cc4ae1aff11a060ccf
jfruitet <jean.fruitet@free.fr>
2016-08-01 10:26:37 +0200

    Initial commit
    All the stuff…

---
+++ lang/en/tool_lpimportreferentiel.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'tool_lpimportreferentiel', language 'en'
+ *
+ * @package    tool_lpimportreferentiel
+ * @copyright  2015 Damyon Wiese
+ * @copyright  2016 Jean Fruitet jean.fruitet@free.fr 
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Import a Skills repository (referentiel plugin) from XML';
+$string['importfile'] = 'Referentiel description file XML format';
+$string['invalidimportfile'] = 'File format is invalid.';
+$string['subject'] = 'Subject: {$a}';
+$string['educationlevel'] = 'Education Level: {$a}';
+$string['import'] = 'Import';
+$string['unnamed'] = 'Unnamed';
+$string['competency'] = 'Competency';
+$string['competencyscale'] = 'Competency Scale: {$a}';
+$string['competencyscaledescription'] = 'Competency scale created by import';
+$string['competencyframeworkexists'] = 'This Competency Framework yet exists...';
